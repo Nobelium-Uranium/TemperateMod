f98fb7f254e5e2c9c2ac02967fe89da04c2355ef
Chem <minecartanimation@gmail.com>
2020-06-03 04:30:48 -0600

    Added Thin Ice Wand

---
+++ Items/Placeable/Special/ThinIceWand.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TemperateMod.Items.Placeable.Special
+{
+    class ThinIceWand : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Places thin ice");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = item.height = 20;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.createTile = TileID.BreakableIce;
+            item.rare = ItemRarityID.Blue;
+            item.value = Item.sellPrice(silver: 25);
+        }
+    }
+}
