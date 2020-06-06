8fa4d919c1123929a296294a1ddd1645eb70b160
Chem <minecartanimation@gmail.com>
2020-06-05 20:55:46 -0600

    Added temporary Glacial Caverns theme

---
+++ Items/Placeable/MusicBox/GlacialCavernsMusicBox.cs
@@ -0,0 +1,30 @@
+﻿using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+
+namespace TemperateMod.Items.Placeable.MusicBox
+{
+    class GlacialCavernsMusicBox : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Music Box (Glacial Caverns)");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.consumable = true;
+            item.createTile = TileType<Tiles.MusicBox.GlacialCavernsMusicBox>();
+            item.width = 24;
+            item.height = 24;
+            item.rare = ItemRarityID.Orange;
+            item.value = 100000;
+            item.accessory = true;
+        }
+    }
+}
--- TemperateMod.cs
+++ TemperateMod.cs
@@ -1,8 +1,16 @@
+using Terraria;
 using Terraria.ModLoader;
 
 namespace TemperateMod
 {
 	public class TemperateMod : Mod
 	{
-	}
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
+                AddMusicBox(GetSoundSlot(SoundType.Music, "Sounds/Music/GlacialCaverns"), ItemType("GlacialCavernsMusicBox"), TileType("GlacialCavernsMusicBox"));
+            }
+        }
+    }
 }
