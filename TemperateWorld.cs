abf69e7f7d20a63ca5a918abcd36f94eb7ce97f0
Chem <minecartanimation@gmail.com>
2020-06-06 22:55:11 -0600

    Glacial Caverns worldgen finally works as intended

--- TemperateWorld.cs
+++ TemperateWorld.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.GameContent.Generation;
 using Terraria.ID;
@@ -17,46 +18,37 @@ namespace TemperateMod
         {
             int PostSnow = tasks.FindIndex(genpass => genpass.Name.Equals("Slush Check"));
             tasks.Insert(PostSnow + 1, new PassLegacy("Glacial Caverns", TemperateGlacialCaverns));
+            int PostShinies = tasks.FindIndex(genpass => genpass.Name.Equals("Shinies"));
+            tasks.Insert(PostShinies + 1, new PassLegacy("Glacier Ores", TemperateGlacierOres));
         }
 
         private void TemperateGlacialCaverns(GenerationProgress progress)
         {
             progress.Message = "Creating Glacial Caverns";
-
             for (int i = 0; i < Main.maxTilesX; i++)
             {
                 for (int j = 0; j < Main.maxTilesY; j++)
                 {
+                    int top = (int)WorldGen.rockLayer + 125;
                     Tile tile = Framing.GetTileSafely(i, j);
-                    if (tile.active() && tile.type == TileID.IceBlock && j == 150)
-                    {
-                        if (Main.rand.NextBool())
-                            Main.tile[i, j].type = (ushort)TileType<Tiles.GlacialCaverns.PermafrostTile>();
-                        if (Main.rand.NextBool(3))
-                            Main.tile[i, j - 1].type = (ushort)TileType<Tiles.GlacialCaverns.PermafrostTile>();
-                    }
-                }
-            }
-
-            for (int k = 0; k < (int)(Main.maxTilesX * Main.maxTilesY * 6E-05); k++)
-            {
-                int x = WorldGen.genRand.Next(0, Main.maxTilesX);
-                int y = WorldGen.genRand.Next((int)WorldGen.rockLayer + 150, Main.maxTilesY);
-                Tile tile = Framing.GetTileSafely(x, y);
-                if (tile.active() && tile.type == TileID.IceBlock)
-                    WorldGen.TileRunner(x, y, WorldGen.genRand.Next(6, 12), WorldGen.genRand.Next(12, 24), TileID.Cobalt); // Cobalt ore is placeholder
-            }
-
-            for (int i = 0; i < Main.maxTilesX; i++)
-            {
-                for (int j = 0; j < Main.maxTilesY; j++)
-                {
-                    Tile tile = Framing.GetTileSafely(i, j);
-                    if (tile.active() && tile.type == TileID.IceBlock && j >= (int)WorldGen.rockLayer + 151)
+                    if (tile.active() && tile.type == TileID.IceBlock && j >= top && Main.rand.Next(0, 21 - Math.Min(j - top, 20)) == 0)
                     {
                         Main.tile[i, j].type = (ushort)TileType<Tiles.GlacialCaverns.PermafrostTile>();
                     }
                 }
+            }
+        }
+
+        private void TemperateGlacierOres(GenerationProgress progress)
+        {
+            progress.Message = "Hiding Frozen Treasure";
+            for (int k = 0; k < (int)(Main.maxTilesX * Main.maxTilesY * 6E-05); k++)
+            {
+                int x = WorldGen.genRand.Next(0, Main.maxTilesX);
+                int y = WorldGen.genRand.Next((int)WorldGen.rockLayer + 125, Main.maxTilesY);
+                Tile tile = Framing.GetTileSafely(x, y);
+                if (tile.active() && tile.type == TileType<Tiles.GlacialCaverns.PermafrostTile>())
+                    WorldGen.TileRunner(x, y, WorldGen.genRand.Next(5, 10), WorldGen.genRand.Next(25, 50), TileID.Cobalt); // Cobalt ore is placeholder
             }
         }
 
